baaa2839a68543bd109d5e8fc4e0f488bc4dd24f
PWareeborisut <Kerry@12345>
2019-07-25 11:58:46 +0700

    add divide

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     plus: (first, second) => first + second,
     subtract: (first, second) => first - second,
-    multiply: (first, second) => first * second
+    multiply: (first, second) => first * second,
+    divide: (first, second) => first / second
 
 }
